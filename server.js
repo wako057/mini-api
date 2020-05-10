4e5fde9fa8b4bd9b190f80dd8585f1e94117c48c
Gregory GERARD <wako057@gmail.com>
2020-05-11 00:32:09 +0200

    feat: mod to allow test on app/supertest

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 "use strict";
 
-const port = process.env.PORT || 8080;
+const port = process.env.NODE_PORT || 4242;
 const config = require("js-config");
 const loggers = require("js-logger").init(config.logs);
-const app = require("./src/app")(config, loggers);
+const app = require("./src/app");
 
-const server = app.getApp().listen(port, () => {
+const server = app.listen(port, () => {
     loggers.logger.info(`
 Process ${process.pid} is listening to all incoming requests on port ${port},
 workerProcess: ${process.pid}`);
 });
-
 
 module.exports = server;
