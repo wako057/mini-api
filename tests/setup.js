ce0b0aaf19fd6ad02dc986de5ead2a438e4b00e7
Gregory GERARD <wako057@gmail.com>
2020-05-11 01:35:53 +0200

    test: add reset command

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,19 @@
 "use strict";
 
+const childProcess = require("child_process");
+const conf = require("js-config").databases;
+
+const execConf = {
+    env: {
+        DB_HOST: conf.host,
+        DB_USER: conf.user,
+        DB_PORT: conf.port,
+        PGPASSWORD: conf.password,
+        DB_NAME: conf.database,
+        PATH: process.env.PATH
+    }
+};
+
 module.exports = () => {
+    return childProcess.execFileSync(`${process.cwd()}/tests/db.up.sh`, execConf);
 };
